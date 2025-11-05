44a94a37e09759489eae1a764d67ef4e851e300f
SantiagoVillagra <sanmavillagra@gmail.com>
2025-11-05 17:33:46 -0300

    subida

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "../styles/modern-normalize.css"
+import "../styles/styles.css"
+import "../styles/componentes/header.css"
+import "../styles/componentes/hero.css"
+import "../styles/componentes/about.css"
+import "../styles/componentes/featured.css"
+import "../styles/componentes/work.css"
+import "../styles/componentes/contact.css"
+import "../styles/componentes/footer.css"
+import "../styles/componentes/mobile-nav.css"
+import "../styles/utils.css"
+
+import mobileNav from "./utils/mobile-nav"
+import darkMode from "./utils/dark-mode"
+import lazyLoading from "./utils/lazy-loading"
+
+mobileNav()
+darkMode()
+lazyLoading()
